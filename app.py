cddf15b10d679bc1b69f7641cf53dc336a1de7b4
TH1707 <114655917+TH1707@users.noreply.github.com>
2023-08-30 15:50:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,33 +3,42 @@ import os
 import pandas as pd
 import joblib as jb
 
+# Define the heading style as HTML
 heading_style = '''
 <div style="color:red;" align='center'>
 <h1>Loan Amount Prediction System</h1>
 </div>
 '''
-def return_df(tv,radio,newspaper):
-    kbn={
-	    'tv':[tv],
-	    'radio':[radio],
-	    'newspaper':[newspaper]
-	    
+
+# Define a function to return the input data as a DataFrame
+def return_df(tv, radio, newspaper):
+    data = {
+        'tv': [tv],
+        'radio': [radio],
+        'newspaper': [newspaper]
     }
-    final_df=pd.DataFrame(kbn)
+    final_df = pd.DataFrame(data)
     return final_df
 
+# Define a function to load the trained model
 def base_model():
-    bmodel=jb.load(os.path.join('finalized_model_regression.pkl'))
-    return bmodel
+    model = jb.load('finalized_model_regression.pkl')
+    return model
 
+# Streamlit UI
 st.markdown(heading_style, unsafe_allow_html=True)
-tv=st.number_input('How Much Money Was Spent On tv')
-radio=st.number_input('How much money was spent on radio')
-newspaper=st.number_input('How much money was spent on newspaper')
-df=return_df(tv,radio,newspaper)
-    
+
+# Input fields for advertising expenses
+tv = st.number_input('How Much Money Was Spent On TV')
+radio = st.number_input('How much money was spent on Radio')
+newspaper = st.number_input('How much money was spent on Newspaper')
+
+# Get the input data as a DataFrame
+df = return_df(tv, radio, newspaper)
+
+# Check if the "Submit" button is clicked
 if st.button('Submit'):
-	model=base_model()
-	preds=model.predict(df)
-	predictions=preds[0]
-	st.write(predictions)
+    model = base_model()
+    preds = model.predict(df)
+    prediction = preds[0]
+    st.write("Predicted Loan Amount:", prediction)
